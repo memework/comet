b64a8325d9e76e6526251ab96bd734873fed4dfc
Rhodesia <carl@mercyfuckers.org>
2017-10-01 22:49:32 +0000

    core now uses the log

--- plugins/core.py
+++ plugins/core.py
@@ -12,26 +12,26 @@ class Core:
 	"""Core functions of the bot, includes plugins"""
 	def __init__(self, bot):
 		self.bot = bot
-		print("loading plugins...")
+		bot.log.info("loading plugins...")
 		try:
 			self.plugins = clark.load('config/plugins.yml')
 		except YAMLError as exc:
 			raise Exception('Invalid yaml, wtf!')
 		except FileNotFoundError:
-			print("file not found, creating new plugin config!")
+			bot.log.warning("file not found, creating new plugin config!")
 			self.plugins = []
 			clark.save(self.plugins, "config/plugins.yml")
 		for i in self.plugins:
 			try:
 				self.bot.load_extension("plugins." + i)
-				print("[ \033[0;32mdone\033[0;0m ] %s loaded sucessfuly" % i)
+				bot.log.info("%s loaded sucessfuly" % i)
 			except Exception as e:
 				self.plugins.remove(i)
-				print("[ \033[1;31mfail\033[0;0m ] {}: {}".format(i, e))
+				bot.log.error("{}: {}".format(i, e))
 				clark.save(self.plugins, "config/plugins.yml")
-				print("[ \033[1;96mwarn\033[0;0m ] {} unloaded due to errors, load it when it works again".format(i))
+				bot.log.warn("{} unloaded due to errors, load it when it works again".format(i))
 
-		print("%i plugins loaded" % len(self.plugins))
+		bot.log.info("%i plugins loaded" % len(self.plugins))
 
 	@checks.is_dev()
 	@commands.command(name="plugins")
@@ -48,12 +48,12 @@ class Core:
 			return
 		try:
 			self.bot.unload_extension("plugins." + name)
-			print("unloaded "+name)
+			self.bot.log.info("unloaded "+name)
 		except Exception as e:
 			await ctx.send("failed to reload plugin: {}".format(e))
 		try:
 			self.bot.load_extension("plugins." + name)
-			print("reloaded "+name+" sucessfully")
+			self.bot.log.info("reloaded "+name+" sucessfully")
 			await ctx.send("plugin reloaded sucessfuly")
 		except Exception as e:
 			await ctx.send("failed to load plugin: {}".format(e))
@@ -76,8 +76,10 @@ class Core:
 			self.plugins.append(name)
 			clark.save(self.plugins, "config/plugins.yml")
 			await ctx.send("plugin loaded sucessfuly")
+			self.bot.log.info("Plugin "+name+" loaded.")
 		except Exception as e:
 			await ctx.send("failed to load plugin: {}".format(e))
+			self.bot.log.error("failed to load plugin: {}".format(e))
 
 	@checks.is_dev()
 	@commands.command()
@@ -91,7 +93,9 @@ class Core:
 			self.plugins.remove(name)
 			clark.save(self.plugins, "config/plugins.yml")
 			await ctx.send("plugin unloaded sucessfuly")
+			self.bot.log.info("plugin "+name+" loaded")
 		except Exception as e:
 			await ctx.send("failed to load plugin: {}".format(e))
+			self.bot.log.error("failed to load plugin: {}".format(e))
 def setup(bot):
 	bot.add_cog(Core(bot))
